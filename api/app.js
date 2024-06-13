4eb70ffbf92198b4ec9b7eba8eb6eca0cc174080
Suren Fernando <60933240+surenfernando@users.noreply.github.com>
2024-06-14 00:48:59 +1000

    chore: setup modular files and routers

--- api/app.js
+++ api/app.js
@@ -1,6 +1,13 @@
 import express from "express";
+import postRoute from "./routes/post.route.js";
 
 const app = express();
+
+app.use("/api/test", (req, res) => {
+  res.send("Hello world");
+});
+
+app.use("/api/posts", postRoute);
 
 app.listen(8800, () => {
   console.log("Server is running on port 8800");
