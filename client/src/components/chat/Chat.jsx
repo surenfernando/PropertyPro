9970502e0c855b7ead1c74200ffaf7d6d8200a64
Suren Fernando <60933240+surenfernando@users.noreply.github.com>
2024-12-12 18:52:46 +1100

    fix: chat box spacing for popup

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -11,43 +11,31 @@ function Chat() {
         <div className="message">
           <img src={userData.img} alt="" />
           <span>John Doe</span>
-          <p>Lorem ipsum dolor sit...</p>
+          <p>Lorem ipsum dolor sit amet...</p>
         </div>
 
         <div className="message">
           <img src={userData.img} alt="" />
           <span>John Doe</span>
-          <p>Lorem ipsum dolor sit...</p>
+          <p>Lorem ipsum dolor sit amet...</p>
         </div>
 
         <div className="message">
           <img src={userData.img} alt="" />
           <span>John Doe</span>
-          <p>Lorem ipsum dolor sit...</p>
+          <p>Lorem ipsum dolor sit amet...</p>
         </div>
 
         <div className="message">
           <img src={userData.img} alt="" />
           <span>John Doe</span>
-          <p>Lorem ipsum dolor sit...</p>
+          <p>Lorem ipsum dolor sit amet...</p>
         </div>
 
         <div className="message">
           <img src={userData.img} alt="" />
           <span>John Doe</span>
-          <p>Lorem ipsum dolor sit...</p>
-        </div>
-
-        <div className="message">
-          <img src={userData.img} alt="" />
-          <span>John Doe</span>
-          <p>Lorem ipsum dolor sit...</p>
-        </div>
-
-        <div className="message">
-          <img src={userData.img} alt="" />
-          <span>John Doe</span>
-          <p>Lorem ipsum dolor sit...</p>
+          <p>Lorem ipsum dolor sit amet...</p>
         </div>
       </div>
 
@@ -55,28 +43,60 @@ function Chat() {
         <div className="chatBox">
           <div className="top">
             <div className="user">
-              <img src={userData.img} alt="" />
+              <img
+                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt=""
+              />
               John Doe
             </div>
-
             <span className="close" onClick={() => setChat(null)}>
               X
             </span>
           </div>
-
           <div className="center">
             <div className="chatMessage">
-              <p>Random message</p>
+              <p>Lorem ipsum dolor sit amet</p>
               <span>1 hour ago</span>
             </div>
-
             <div className="chatMessage own">
-              <p>Random message</p>
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit amet</p>
               <span>1 hour ago</span>
             </div>
           </div>
           <div className="bottom">
-            <textarea name="" id=""></textarea>
+            <textarea></textarea>
             <button>Send</button>
           </div>
         </div>
